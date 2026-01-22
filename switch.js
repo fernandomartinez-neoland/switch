fca711fd8cb408a588f01a316a0e99d83bdfcbbe
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-01-22 12:56:32 +0100

    linear-gradient con switch y selector

--- switch.js
+++ switch.js
@@ -19,3 +19,15 @@ let numero = 5
 console.log("este es el valor original de 'numero'", numero)
 console.log("esto es una comparacion con ==", numero == "5")//correcto
 console.log("esto es una comparacion con ===", numero === 5)//incorrecto
+const color1 = document.getElementById("color1");
+const color2 = document.getElementById("color2");
+
+color1.addEventListener("input", function () {
+
+    document.body.style.background = `linear-gradient(360deg, ${color1.value}, ${color2.value}) no-repeat fixed`;
+})
+
+color2.addEventListener("input", function () {
+
+    document.body.style.background = `linear-gradient(360deg, ${color1.value}, ${color2.value}) no-repeat fixed`;
+})
