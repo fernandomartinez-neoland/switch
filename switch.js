ba83f51d18caa5b36abcb7d64dddf8b397a4fbd7
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-01-22 11:51:26 +0100

    switch

---
+++ switch.js
@@ -0,0 +1,15 @@
+const color = "azul";
+
+switch (color) {
+    case 'amarillo':
+        console.log("amarillo");
+        break;
+    case 'rojo':
+        console.log("rojo");
+        break;
+    case 'verde':
+        console.log("verde");
+        break;
+    default:
+        console.log("color no encontrado");
+}
